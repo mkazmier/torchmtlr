34228c332c4bb2dc6007be223cc95a9b1273265c
mkazmier <mkazmierski.poznan@gmail.com>
2020-10-17 14:15:26 +0200

    Clean up unused functions in utils

--- torchmtlr/utils.py
+++ torchmtlr/utils.py
@@ -3,7 +3,6 @@ from typing import Optional, Union
 
 import numpy as np
 import torch
-from lifelines import KaplanMeierFitter
 
 
 def encode_survival(time: Union[float, int, np.ndarray],
@@ -137,78 +136,3 @@ def make_optimizer(opt_cls: torch.optim.Optimizer,
         {"params": mtlr_weights, "weight_decay": 0.},
     ], **kwargs)
     return optimizer
-
-
-def normalize(data, mean=None, std=None, skip_cols=[]):
-    """Normalizes the columns of Pandas DataFrame to zero mean and unit
-    standard deviation."""
-    if mean is None:
-        mean = data.mean(axis=0)
-    if std is None:
-        std = data.std(axis=0)
-    if skip_cols is not None:
-        mean[skip_cols] = 0
-        std[skip_cols] = 1
-    return (data - mean) / std, mean, std
-
-
-def integrated_brier_score(time_true, time_pred, event_observed, time_bins):
-    r"""Compute the integrated Brier score for a predicted survival function.
-
-    The integrated Brier score is defined as the mean squared error between
-    the true and predicted survival functions at time t, integrated over all
-    timepoints.
-
-    Parameters
-    ----------
-    time_true : np.ndarray, shape=(n_samples,)
-        The true time to event or censoring for each sample.
-    time_pred : np.ndarray, shape=(n_samples, n_time_bins)
-        The predicted survival probabilities for each sample in each time bin.
-    event_observed : np.ndarray, shape=(n_samples,)
-        The event indicator for each sample (1 = event, 0 = censoring).
-    time_bins : np.ndarray, shape=(n_time_bins,)
-        The time bins for which the survival function was computed.
-
-    Returns
-    -------
-    float
-        The integrated Brier score of the predictions.
-
-    Notes
-    -----
-    This function uses the definition from [1]_ with inverse probability
-    of censoring weighting (IPCW) to correct for censored observations.
-    The weights are computed using the Kaplan-Meier estimate of the censoring
-    distribution.
-
-    References
-    ----------
-    .. [1] E. Graf, C. Schmoor, W. Sauerbrei, and M. Schumacher, ‘Assessment
-       and comparison of prognostic classification schemes for survival data’,
-       Statistics in Medicine, vol. 18, no. 17‐18, pp. 2529–2545, Sep. 1999.
-    """
-
-    # compute weights for inverse probability of censoring weighting (IPCW)
-    censoring_km = KaplanMeierFitter()
-    censoring_km.fit(time_true, 1 - event_observed)
-    weights_event = 1 / censoring_km.survival_function_at_times(
-        time_true).values.reshape(-1, 1)
-    weights_no_event = 1 / censoring_km.survival_function_at_times(
-        time_bins).values.reshape(1, -1)
-    weights_event[np.isinf(weights_event)] = 0
-    weights_no_event[np.isinf(weights_no_event)] = 0
-
-    # scores for subjects with event before time t for each time bin
-    uncensored = (time_true[:, np.newaxis] <
-                  time_bins) & event_observed[:, np.newaxis].astype(np.bool)
-    scores_event = np.where(uncensored, (-time_pred)**2 * weights_event, 0)
-    # scores for subjects with no event and no censoring before time t for each time bin
-    scores_no_event = np.where((time_true[:, np.newaxis] >= time_bins),
-                               (1 - time_pred)**2 * weights_no_event, 0)
-
-    scores = np.mean(scores_event + scores_no_event, axis=0)
-
-    # integrate over all time bins
-    score = np.trapz(scores, time_bins) / time_bins.max()
-    return score
